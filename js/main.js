b09de472df2cd5a7b5d9b2897c01a84a05698017
Noha Haidar <n_haidar@mail.fhsu.edu>
2024-12-09 13:09:18 -0600

    Function 13

--- js/main.js
+++ js/main.js
@@ -169,5 +169,18 @@ async function getUser(userId) {
     }
 }
 
+//Function 13
+async function getPostComments(postId) {
+    if (!postId) return undefined;
+
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+        const comments = await response.json();
+        return comments;
+    } catch (error) {
+        console.error('Error fetching post comments:', error);
+    }
+}
+
 
 
